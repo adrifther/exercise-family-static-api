e6e39af0ed5bc255e33e5fbdbea47d622e2adc14
Adri <adrianferrert@gmail.com>
2025-06-12 15:09:39 +0000

    first and last commit

--- src/datastructures.py
+++ src/datastructures.py
@@ -16,6 +16,20 @@ class FamilyStructure:
                 "last_name": last_name,
                 "age": 33,
                 "lucky_numbers": [7, 13, 22]
+            },
+            {
+                "id": self._generate_id(),
+                "first_name": "Jane",
+                "last_name": last_name,
+                "age": 35,
+                "lucky_numbers": [10, 14, 3]
+            },
+            {
+                "id": self._generate_id(),
+                "first_name": "Jimmy",
+                "last_name": last_name,
+                "age": 5,
+                "lucky_numbers": [1]
             }
         ]
 
@@ -26,20 +40,30 @@ class FamilyStructure:
         return generated_id
 
     def add_member(self, member):
-        ## You have to implement this method
-        ## Append the member to the list of _members
-        pass
+        member["id"] = self._generate_id()
+        self._members.append(member)
+        return member
+        
 
     def delete_member(self, id):
         ## You have to implement this method
         ## Loop the list and delete the member with the given id
-        pass
+        initial_len = len(self._members)
+        self._members = [m for m in self._members if m["id"] != id]
+        return {"done": True} if len(self._members) < initial_len else {"done": False}
 
     def get_member(self, id):
         ## You have to implement this method
         ## Loop all the members and return the one with the given id
-        pass
-
+        for member in self._members:
+            if member["id"] == id:
+                return {
+                    "id": member["id"],
+                    "first_name": member["first_name"],
+                    "age": member["age"],
+                    "lucky_numbers": member["lucky_numbers"]
+                }
+    
     # This method is done, it returns a list with all the family members
     def get_all_members(self):
         return self._members
